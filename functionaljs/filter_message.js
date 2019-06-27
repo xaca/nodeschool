d660a44d44892e05231e5bc3a1adb7fe78481069
xaca <and1res@hotmail.com>
2019-06-26 19:19:40 -0500

    Borrando archivos creados por error

--- functionaljs/filter_message.js
+++ functionaljs/filter_message.js
@@ -1,6 +1,7 @@
+var t = [ { message: 'Dolore laborum amet deserunt amet aute enim.' }, { message: 'Quis velit ad in ut eiusmod amet ea enim eiusmod.' }, { message: 'Pariatur ut aute eu eu nisi ea ea.' }, { message: 'Nisi do Lorem anim in sint in ut eiusmod.' }, { message: 'Aliquip anim magna nulla aliquip.' }, { message: 'Lorem ullamco tempor exercitation veniam.' }, { message: 'Veniam aliquip nisi magna incididunt.' } ];
 function getShortMessages(messages) {
     // SOLUTION GOES HERE
     return messages.filter(m=> m.message.length <50);
   }
-  
+  getShortMessages(t);
   module.exports = getShortMessages
